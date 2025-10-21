cfc8ac4dae595ad19f09826ea6430074e65251ff
Tian-Shi Xu <994876761@qq.com>
2025-10-21 16:18:28 +0800

    .

--- cs336_systems/leaderboard.py
+++ cs336_systems/leaderboard.py
@@ -5,6 +5,7 @@ from einops import rearrange,einsum,reduce
 import triton
 import triton.language as tl
 from flash_att import FlashAttentionTriton
+from triton_tutorial import _attention
 from torch.nn.functional import scaled_dot_product_attention
 
 def test_timing_flash_forward_backward():
@@ -12,17 +13,17 @@ def test_timing_flash_forward_backward():
     d_head = 64
     sequence_length = 16384
     q, k, v = torch.randn(
-        3,1, n_heads, sequence_length, d_head, device='cuda', dtype=torch.bfloat16, requires_grad=True
+        3,1, n_heads, sequence_length, d_head, device='cuda', dtype=torch.float16, requires_grad=True
     )
 
-    flash = torch.compile(FlashAttentionTriton.apply)
+    flash = torch.compile(_attention.apply)
     # flash = scaled_dot_product_attention
 
     def flash_forward_backward():
         # print("q.shape",q.shape)
-        o = flash(q, k, v, False)
-        # loss = o.sum()
-        # loss.backward()
+        o = flash(q, k, v, False, 1/math.sqrt(d_head))
+        do = torch.randn_like(o, dtype=torch.float32, device='cuda')
+        o.backward(do)
     results = triton.testing.do_bench(flash_forward_backward, rep=10000, warmup=1000)
     print(results)
     
