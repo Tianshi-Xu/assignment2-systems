eb7512aaf712fef971ee8ba4b7ed79b0afb33dfa
Tianshi Xu <994876761@qq.com>
2025-10-22 10:34:26 +0000

    .

--- nsp.sh
+++ nsp.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=2 uv run nsys profile --force-overwrite true --python-backtrace=cuda -o result_torch python cs336_systems/flash_att.py
+CUDA_VISIBLE_DEVICES=2 uv run nsys profile --force-overwrite true --python-backtrace=cuda -o result_leaderboard python cs336_systems/flash_att.py
